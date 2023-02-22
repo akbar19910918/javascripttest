e1f24b19b469224876da5b4f3f5e6d7373516c16
akbar19910918 <akbarachilov91@gmail.com>
2023-02-22 17:54:51 +0500

    test

---
+++ index.js
@@ -0,0 +1,35 @@
+//string=========
+//number=======
+//undefined=======
+//null======
+//boolen====
+//bigInt======
+//symboll=====
+
+//object=======
+
+
+//string------ "",'',``
+// var;let; const:
+//var text="salom xammaga.xamma yaxshimi";
+//console.log( text);
+//let text="salom dostlar";
+//console.log(text);
+
+//number-----number;Nan;infinity;
+
+//undefined------bo'sh maydon;
+
+//null-------bo'shab qolgan maydon;
+
+//boolen----- true,false;
+
+//bigInt----katta son
+// 9007199254740992
+//symbol----kalit soz;
+
+//object------malumotlar bazasi;
+//arrey--[]
+//object--{}
+//function---(){}
+
